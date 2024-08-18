56ab2b4daff3120f92655bbe7d9c5be70b149730
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-08-18 21:35:54 +0300

    Solving all bugs but the edit profile and change password doesn't work

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -7,11 +7,17 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("index/", views.index, name="index"),
-    path("error/", views.custom_404, name="error"),
-    path("login/", views.login, name="login"),
-    path("register/", views.register, name="register"),
-    path("profile/", views.profile, name="profile"),
+    path("index", views.index, name="index"),
+    path("index.html", views.index, name="index"),
+    path("error", views.custom_404, name="custom_error"),
+    path("error.html", views.custom_404, name="error"),
+    path("login", views.login, name="login"),
+    path("login.html", views.login, name="login"),
+    path("register", views.register, name="register"),
+    path("register.html", views.register, name="register"),
+    path("profile", views.profile, name="profile"),
+    path("profile.html", views.profile, name="profile"),
+    path("logout/", views.logout, name="logout"),
 ]
 
 if settings.DEBUG:
